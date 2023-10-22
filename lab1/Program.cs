88107e1ed6547b5a7a620fb16a25496cad588d75
de-logun <60477352+de-logun@users.noreply.github.com>
2023-10-22 18:17:50 +0300

    lab1 done

---
+++ lab1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        if (!File.Exists("INPUT.TXT"))
+        {
+            Console.WriteLine("Вхідний файл не знайдено.");
+            return;
+        }
+
+        using (StreamReader reader = new StreamReader("INPUT.TXT"))
+        {
+            string line = reader.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                Console.WriteLine("Вхідний файл порожній.");
+                return;
+            }
+
+            string[] input = line.Split();
+
+            if (input.Length != 3)
+            {
+                Console.WriteLine("Вхідний файл має неправильний формат.");
+                return;
+            }
+
+            if (!int.TryParse(input[0], out int a) || !int.TryParse(input[1], out int b) || !int.TryParse(input[2], out int c))
+            {
+                Console.WriteLine("Помилка введення значень, формат: число а число б число с");
+                return;
+            }
+
+            string x = a.ToString();
+            string y = b.ToString();
+
+            char[] xArray = x.ToCharArray();
+            char[] yArray = y.ToCharArray();
+
+            Array.Sort(xArray);
+            Array.Sort(yArray);
+
+            bool permutationFound = false;
+
+            do
+            {
+                x = new string(xArray);
+                y = new string(yArray);
+                a = int.Parse(x);
+                if (a > c) break;
+
+                string z = (c - a).ToString().PadLeft(y.Length, '0');
+
+                char[] zArray = z.ToCharArray();
+                Array.Sort(zArray);
+                z = new string(zArray);
+
+                if (z == y)
+                {
+                    permutationFound = true;
+                    break;
+                }
+            } while (NextPermutation(xArray));
+
+            using (StreamWriter writer = new StreamWriter("OUTPUT.TXT"))
+            {
+                if (permutationFound)
+                {
+                    writer.WriteLine("YES");
+                    writer.WriteLine($"{a} {c - a}");
+                }
+                else
+                {
+                    writer.WriteLine("NO");
+                }
+            }
+        }
+    }
+
+    static bool NextPermutation(char[] array)
+    {
+        int i = array.Length - 2;
+        while (i >= 0 && array[i] >= array[i + 1])
+            i--;
+
+        if (i < 0)
+            return false;
+
+        int j = array.Length - 1;
+        while (array[j] <= array[i])
+            j--;
+
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        Array.Reverse(array, i + 1, array.Length - i - 1);
+        return true;
+    }
+}
